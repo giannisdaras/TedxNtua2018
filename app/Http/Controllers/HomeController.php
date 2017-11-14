d82bb0a4df56eea7ea8672754973143c71411e17
Theo Diamantidis <diamaltho@gmail.com>
2017-11-15 01:27:41 +0200

    First try with the layout and the Bootstrap navbar component; TODO: hot-dog icon doesn't work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,48 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
+    	$navlinks = array(
+    		'home' => array(
+    			'url' => url('/'),
+    			'title' => 'Home',
+    			'active' => true
+    		),
+    		'partners' => array(
+    			'url' => url('/partners'),
+    			'title' => 'Partners',
+    			'active' => false
+    		),
+    		'speakers' => array(
+    			'url' => url('/#speakers'),
+    			'title' => 'Speakers',
+    			'active' => false
+    		),
+    		'schedule' => array(
+    			'url' => url('/schedule'),
+    			'title' => 'Schedule',
+    			'active' => false
+    		),
+    		'team' => array(
+    			'url' => url('/team'),
+    			'title' => 'Team',
+    			'active' => false
+    		),
+    		'about' => array(
+    			'url' => url('/about'),
+    			'title' => 'About',
+    			'active' => false
+    		),
+    		'contact' => array(
+    			'url' => url('/contact'),
+    			'title' => 'Contact',
+    			'active' => false
+    		),
+    		'get_tickets' => array(
+    			'url' => 'https://www.eventora.com',
+    			'title' => 'Get Tickets',
+    			'active' => false
+    		),
+    	);
+        return view('home')->with('navlinks', $navlinks);
     }
 }
