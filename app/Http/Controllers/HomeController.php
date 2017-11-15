db2a8c98dd42a07cdee4f8df3807978efaa947da
Theo Diamantidis <diamaltho@gmail.com>
2017-11-15 13:56:20 +0200

    Working draft of navigation menu; Hide FUSE files with .gitignore; Make navigation link array static and remove it from the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,48 +7,6 @@ class HomeController extends Controller
 
     public function index()
     {
-    	$navlinks = array(
-    		'home' => array(
-    			'url' => url('/'),
-    			'title' => 'Home',
-    			'active' => true
-    		),
-    		'partners' => array(
-    			'url' => url('/partners'),
-    			'title' => 'Partners',
-    			'active' => false
-    		),
-    		'speakers' => array(
-    			'url' => url('/#speakers'),
-    			'title' => 'Speakers',
-    			'active' => false
-    		),
-    		'schedule' => array(
-    			'url' => url('/schedule'),
-    			'title' => 'Schedule',
-    			'active' => false
-    		),
-    		'team' => array(
-    			'url' => url('/team'),
-    			'title' => 'Team',
-    			'active' => false
-    		),
-    		'about' => array(
-    			'url' => url('/about'),
-    			'title' => 'About',
-    			'active' => false
-    		),
-    		'contact' => array(
-    			'url' => url('/contact'),
-    			'title' => 'Contact',
-    			'active' => false
-    		),
-    		'get_tickets' => array(
-    			'url' => 'https://www.eventora.com',
-    			'title' => 'Get Tickets',
-    			'active' => false
-    		),
-    	);
-        return view('home')->with('navlinks', $navlinks);
+        return view('home');
     }
 }
