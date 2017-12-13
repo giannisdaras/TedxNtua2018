34da16944cfdcca1145602c6e840aabcf8145fcb
Theo Diamantidis <diamaltho@gmail.com>
2017-12-13 20:04:34 +0200

    Draft for schedule banner; Change font to add Greek support; Fix logo; Add translations for home, nav, footer and schedule

--- resources/lang/el/general.php
+++ resources/lang/el/general.php
@@ -15,4 +15,56 @@ return [
 
     'language' => 'Ελληνικά',
 
+    /* NAV */
+    'nav' => array(
+        'home' => 'Αρχική',
+        'partners' => 'Χορηγοί',
+        'speakers' => 'Ομιλητές',
+        'schedule' => 'Πρόγραμμα',
+        'team' => 'Ομάδα',
+        'blog' => 'Blog',
+        'about' => 'Σχετικά με μας',
+        'contact' => 'Επικοινωνία',
+        'gettickets' => 'Κάνε κράτηση'
+    ),
+
+    /* FOOTER */
+    'footer' => array(
+        'facebook_title' => 'Ροή από Facebook',
+        'instagram_title' => 'Ροή από Instagram',
+        'past_events_title' => 'Προηγούμενα έτη',
+        'youtube_title' => 'Βίντεο προηγούμενων TEDxNTUA στο YouTube',
+        'handcrafted' => 'Φτιαγμένο με'
+    ),
+
+
+    /* HOME */
+    'hero' => array(
+        'date' => '24 Μαρτίου 2018',
+        'venue' => 'Ωδείο Αθηνών',
+        'address' => 'Ρηγίλλης & Βασ. Γεωργίου Β 17-19',
+        'tickets' => 'Κάνε κράτηση τώρα'
+    ),
+    'home_sections' => array(
+        'schedule' => array(
+            'title' => 'Πρόγραμμα',
+            'desc' => 'Η ουσία μίας εκδήλωσης TEDx κρύβεται στο μίγμα των <strong>ομιλιών</strong>, των <strong>παραστάσεων</strong> και των <strong>workshops</strong> που περιλαμβάνει. Ανακαλύψτε τους τομείς που θα εξερευνήσουμε και ελάτε μαζί μας σε ένα χαοτικό ταξίδι.',
+            'calltoaction' => 'Δείτε το πλήρες πρόγραμμα'
+        ),
+        'map' => array(
+            'title' => 'Χάρτης',
+            'desc' => 'Το TEDxNTUA 2018 θα διεξαχθεί στο Ωδείο Αθηνών. Το Ωδείο βρίσκεται στην οδό Ρηγίλλης &amp; Βασιλέως Γεωργίου Β 17-19, μόλις λιγότερο από 10 λεπτά με τα πόδια από τον σταθμό μετρό <strong>Ευαγγελισμός</strong>.'
+        )
+    ),
+
+    /* SCHEDULE */
+    'schedule' => array(
+        'title' => 'Πρόγραμμα',
+        'time' => 'Ώρα',
+        'talks' => 'Ομιλίες',
+        'performances' => 'Παραστάσεις',
+        'workshops' => 'Workshops'
+    )
+
+    /* don't forget to omit the last comma */
 ];
--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -15,4 +15,56 @@ return [
 
     'language' => 'English',
 
+    /* NAV */
+    'nav' => array(
+        'home' => 'Home',
+        'partners' => 'Partners',
+        'speakers' => 'Speakers',
+        'schedule' => 'Schedule',
+        'team' => 'Team',
+        'blog' => 'Blog',
+        'about' => 'About',
+        'contact' => 'Contact',
+        'gettickets' => 'Get Tickets'
+    ),
+
+    /* FOOTER */
+    'footer' => array(
+        'facebook_title' => 'Facebook stream',
+        'instagram_title' => 'Instagram stream',
+        'past_events_title' => 'Past events',
+        'youtube_title' => 'Videos from past events on YouTube',
+        'handcrafter' => 'Handcrafted with'
+    ),
+
+
+    /* HOME */
+    'hero' => array(
+        'date' => 'March 24, 2018',
+        'venue' => 'Athens Conservatoire',
+        'address' => 'Rigillis & Vas. Georgiou B 17-19',
+        'tickets' => 'Get your tickets now'
+    ),
+    'home_sections' => array(
+        'schedule' => array(
+            'title' => 'Schedule',
+            'desc' => 'The very essence of a TEDx event is its medley of <strong>talks</strong>, <strong>performances</strong> and <strong>workshops</strong>. Discover the topics that will be explored and join us in a journey of Chaos.',
+            'calltoaction' => 'See the full schedule'
+        ),
+        'map' => array(
+            'title' => 'Map & Venue',
+            'desc' => 'TEDxNTUA 2018 will take place at the Athens Conservatoire. The Conservatoire located at Rigillis &amp; Vasileos Georgiou B 17-19, just under 10 minutes on foot from <strong>Evangelismos</strong> subway station.'
+        )
+    ),
+
+    /* SCHEDULE */
+    'schedule' => array(
+        'title' => 'Schedule',
+        'time' => 'Time',
+        'talks' => 'Talks',
+        'performances' => 'Performances',
+        'workshops' => 'Workshops'
+    )
+
+    /* don't forget to omit the last comma */
 ];
