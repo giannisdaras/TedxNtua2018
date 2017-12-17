4162d87cc3d9d63ea1edd2c366e9ae3e70654cbb
Theo Diamantidis <diamaltho@gmail.com>
2017-12-18 00:07:17 +0200

    Completed translation

--- resources/lang/el/general.php
+++ resources/lang/el/general.php
@@ -34,42 +34,61 @@ return [
         'instagram_title' => 'Ροή από Instagram',
         'past_events_title' => 'Προηγούμενα έτη',
         'youtube_title' => 'Βίντεο προηγούμενων TEDxNTUA στο YouTube',
-        'handcrafted' => 'Φτιάχτηκε με'
+        'handcrafted' => 'Φτιάχτηκε με',
+        'license' => 'Αυτή η ανεξάρτητα οργανωμένη εκδήλωση TEDx λειτουργεί με άδεια από το TED.'
     ),
 
 
     /* HOME */
-    'hero' => array(
-        'date' => '24 Μαρτίου 2018',
-        'venue' => 'Ωδείο Αθηνών',
-        'address' => 'Ρηγίλλης & Βασ. Γεωργίου Β 17-19',
-        'tickets' => 'Κάντε κράτηση τώρα'
-    ),
-    'home_sections' => array(
-        'schedule' => array(
-            'title' => 'Πρόγραμμα',
-            'desc' => 'Όλη η ουσία μίας εκδήλωσης TEDx κρύβεται στο μίγμα των <strong>ομιλιών</strong>, των <strong>παραστάσεων</strong> και των <strong>workshops</strong> που περιλαμβάνει. Ανακαλύψτε τους τομείς που θα εξερευνήσουμε και ελάτε μαζί μας σε ένα χαοτικό ταξίδι.',
-            'calltoaction' => 'Δείτε το πλήρες πρόγραμμα'
+    'home' => array(
+        'title' => 'Αρχική',
+        'description' => '',
+        'hero' => array(
+            'date' => '24 Μαρτίου 2018',
+            'venue' => 'Ωδείο Αθηνών',
+            'address' => 'Ρηγίλλης & Βασ. Γεωργίου Β 17-19',
+            'tickets' => 'Κάντε κράτηση τώρα'
         ),
-        'speakers' => array(
-            'title' => 'Ομιλητές',
-            'desc' => 'Άνθρωποι επιστήμονες, επαγγελματίες, καλλιτέχνες. Άτομα με <strong>πάθος</strong> για αυτό που κάνουν και με <strong>ιδέες</strong> που αξίζει να διαδοθούν. Αυτοί είναι οι πρωταγωνιστές μας. Γνωρίστε τους πριν την εκδήλωση και ανακαλύψτε ποιο είναι το έργο τους μέχρι σήμερα.',
-            'calltoaction' => 'Δείτε όλους τους ομιλητές'
-        ),
-        'about' => array(
-            'title' => 'Τι είναι το Chaos;',
-            'desc' => '&ldquo;<i>Χάος είναι οτιδήποτε με το οποίο έχουμε χάσει κάθε επαφή. Γι\' αυτό του έχουμε δώσει κακή έννοια. Το φοβάται το κυρίαρχο αρχέτυπο αυτού του κόσμου, το Εγώ, και σφίγγεται, γιατί η ύπαρξη αυτού ορίζεται υπό όρους ελέγχου.</i>&rdquo; ~Terence McKenna',
-            'calltoaction' => 'Ποιοι είμαστε;'
-        ),
-        'sponsors' => array(
-            'title' => 'Χορηγοί',
-            'desc' => 'Η εκδήλωσή μας δεν θα ήταν εφικτή χωρίς τη στήριξη από οργανισμούς που συμμερίζονται τις <strong>αξίες</strong> και τα <strong>οράματά</strong> μας. Επισκεφθείτε τις ιστοσελίδες τους και μάθετε περισσότερα για αυτούς. Οι μεγαλύτεροι χορηγοί μας παρατίθενται παρακάτω.',
-            'calltoaction' => 'Δείτε όλους τους χορηγούς μας'
-        ),
-        'map' => array(
-            'title' => 'Χάρτης',
-            'desc' => 'Το TEDxNTUA 2018 θα διεξαχθεί στο Ωδείο Αθηνών. Το Ωδείο βρίσκεται στην οδό Ρηγίλλης &amp; Βασιλέως Γεωργίου Β 17-19, μόλις λιγότερο από 10 λεπτά με τα πόδια από τον σταθμό μετρό <strong>Ευαγγελισμός</strong>.'
+        'sections' => array(
+            'schedule' => array(
+                'title' => 'Πρόγραμμα',
+                'desc' => 'Όλη η ουσία μίας εκδήλωσης TEDx κρύβεται στο μίγμα των <strong>ομιλιών</strong>, των <strong>παραστάσεων</strong> και των <strong>workshops</strong> που περιλαμβάνει. Ανακαλύψτε τους τομείς που θα εξερευνήσουμε και ελάτε μαζί μας σε ένα χαοτικό ταξίδι.',
+                'calltoaction' => 'Δείτε το πλήρες πρόγραμμα'
+            ),
+            'speakers' => array(
+                'title' => 'Ομιλητές',
+                'desc' => 'Άνθρωποι επιστήμονες, επαγγελματίες, καλλιτέχνες. Άτομα με <strong>πάθος</strong> για αυτό που κάνουν και με <strong>ιδέες</strong> που αξίζει να διαδοθούν. Αυτοί είναι οι πρωταγωνιστές μας. Γνωρίστε τους πριν την εκδήλωση και ανακαλύψτε ποιο είναι το έργο τους μέχρι σήμερα.',
+                'calltoaction' => 'Δείτε όλους τους ομιλητές'
+            ),
+            'about' => array(
+                'title' => 'Τι είναι το Chaos;',
+                'desc' => '&ldquo;<i>Χάος είναι οτιδήποτε με το οποίο έχουμε χάσει κάθε επαφή. Γι\' αυτό του έχουμε δώσει κακή έννοια. Το φοβάται το κυρίαρχο αρχέτυπο αυτού του κόσμου, το Εγώ, και σφίγγεται, γιατί η ύπαρξη αυτού ορίζεται υπό όρους ελέγχου.</i>&rdquo; ~Terence McKenna',
+                'calltoaction' => 'Ποιοι είμαστε;'
+            ),
+            'sponsors' => array(
+                'title' => 'Χορηγοί',
+                'desc' => 'Η εκδήλωσή μας δεν θα ήταν εφικτή χωρίς τη στήριξη από οργανισμούς που συμμερίζονται τις <strong>αξίες</strong> και τα <strong>οράματά</strong> μας. Επισκεφθείτε τις ιστοσελίδες τους και μάθετε περισσότερα για αυτούς. Οι μεγαλύτεροι χορηγοί μας παρατίθενται παρακάτω.',
+                'calltoaction' => 'Δείτε όλους τους χορηγούς μας'
+            ),
+            'map' => array(
+                'title' => 'Χάρτης',
+                'desc' => 'Το TEDxNTUA 2018 θα διεξαχθεί στο Ωδείο Αθηνών. Το Ωδείο βρίσκεται στην οδό Ρηγίλλης &amp; Βασιλέως Γεωργίου Β 17-19, μόλις λιγότερο από 10 λεπτά με τα πόδια από τον σταθμό μετρό <strong>Ευαγγελισμός</strong>.'
+            )
         )
+    ),
+
+    /* SPONSORS */
+    'sponsors' => array(
+        'title' => 'Χορηγοί',
+        'rank' => array('Μεγάλοι Χορηγοί', 'Συνεργάτες', 'Υποστηρικτές', 'Χορηγοί επικοινωνίας')
+    ),
+
+    /* SPEAKERS */
+    'speakers' => array(
+        'title' => 'Ομιλητές',
+        'back' => 'Πίσω στους Ομιλητές',
+        'short_bio' => 'Σύντομο βιογραφικό',
+        'talk' => 'Ομιλία'
     ),
 
     /* SCHEDULE */
@@ -79,6 +98,76 @@ return [
         'talks' => 'Ομιλίες',
         'performances' => 'Παραστάσεις',
         'workshops' => 'Workshops'
+    ),
+
+    /* TEAM */
+    'team' => array(
+        'title' => 'Ομάδα',
+        'experience' => 'Ομάδα Experience',
+        'fr' => 'Ομάδα Fundraising',
+        'graphics' => 'Ομάδα Graphics',
+        'it' => 'Ομάδα IT',
+        'media' => 'Ομάδα Media',
+        'speakers' => 'Ομάδα Speakers',
+        'venue_production' => 'Ομάδα Venue & Production'
+    ),
+
+    /* BLOG */
+    'blog' => array(
+        'title' => 'Blog'
+    ),
+
+    /* ABOUT */
+    'about' => array(
+        'title' => 'Σχετικά με μας',
+        'tedx' => array(
+            'title' => 'Τι είναι το TEDx;',
+            'subtitle' => 'x = ανεξάρτητα οργανωμένη εκδήλωση',
+            'content' => '<p class="text-justify">In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)</p>'
+        ),
+        'ted' => array(
+            'title' => 'Τι είναι το TED;',
+            'content' => '<p class="text-justify">
+                        TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 30 years ago, TED has grown to support its mission with multiple initiatives. The two annual TED Conferences invite the world\'s leading thinkers and doers to speak for 18 minutes or less. Many of these talks are then made available, free, at TED.com. TED speakers have included Bill Gates, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson, Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Sal Khan and Daniel Kahneman.
+                    </p>    
+                    <p class="text-justify">
+                        The annual TED Conference takes place each spring in Vancouver, British Columbia. TED\'s media initiatives include TED.com, where new TED Talks are posted daily; the Open Translation Project, which provides subtitles and interactive transcripts as well as translations from volunteers worldwide; the educational initiative TED-Ed. TED has established the annual TED Prize, where exceptional individuals with a wish to change the world get help translating their wishes into action; TEDx, which supports individuals or groups in hosting local, self- organized TED-style events around the world, and the TED Fellows program, helping world-changing innovators from around the globe to amplify the impact of their remarkable projects and activities.<br>
+
+                        Follow TED on Twitter at <a href="http://twitter.com/TEDTalks">http://twitter.com/TEDTalks</a>, or on Facebook at <a href="http://www.facebook.com/TED">http://www.facebook.com/TED</a>.    
+                    </p>'
+        ),
+        'tedxntua' => array(
+            'title' => 'Τι είναι το TEDxNTUA;',
+            'content' => '<p class="text-justify">
+                        The various activities of the students and the academic community in general in the National Technical University of Athens are proof of creativity endemic. This scientific potential is the fundamental material for the creation of progressive ideas, which are capable of following the current advancements in science and technology.
+                    </p>
+                    <p class="text-justify">    
+                        Nonetheless, the massive immigration of young graduates towards the flourishing economies of the world, the appearance of numerous new fields, the need for highly specialized scientists and employees, the general sense of individualism and especially the pervasive economical and ethical crisis have deprived the academic community of the collaborative spirit necessary in every learning experience.
+                    </p>    
+                    <p class="text-justify">    
+                        TEDxNTUA is an organized effort to create and sustain the necessary frame for the exchange of new ideas in the fields of technology and business administration. At the same time, NTUA’s community is becoming a living, extrovert organism focusing on the scientific and career goals of its members, while promoting the expan- sion of the network of collaboration between the NTUA, the industries and the startup community.
+                    </p>'
+        )
+    ),
+
+    /* CONTACT */
+    'contact' => array(
+        'title' => 'Επικοινωνία',
+        'contactus' => 'Επικοινωνία',
+        'greeting' => 'Παρακαλούμε δείτε παρακάτω τα στοιχεία επικοινωνίας μας ή απλά συμπληρώστε τη φόρμα και θα σας απαντήσουμε όσο το δυνατόν συντομότερα!',
+        'form' => array(
+            'name' => 'Όνομα',
+            'name_ph' => 'Εισάγετε το όνομά σας',
+            'name_help' => 'Το όνομά σας είναι απαραίτητο',
+            'email' => 'Διεύθυνση e-mail',
+            'email_ph' => 'Εισάγετε το e-mail σας',
+            'email_help' => 'Το e-mail σας είναι απαραίτητο',
+            'subject' => 'Θέμα',
+            'subject_ph' => 'Εισάγετε το θέμα του μηνύματος',
+            'subject_help' => 'Παρακαλούμε εισάγετε ένα κατάλληλο θέμα',
+            'message' => 'Μήνυμα',
+            'submit' => 'Αποστολή'
+        )
     )
 
     /* don't forget to omit the last comma */
