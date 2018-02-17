044421fe58baa3694e14e04ac658bc112d8c75d6
Theo Diamantidis <diamaltho@gmail.com>
2018-02-17 05:28:25 +0200

    Lang switcher closes on click-away or ESC press

--- public/js/app.js
+++ public/js/app.js
@@ -37305,7 +37305,19 @@ $(document).ready(function () {
 	$(document).on("click", "#lang-switch", function (e) {
 
 		e.preventDefault();
-		$($(this).attr("aria-controls")).fadeToggle();
+		$("#lang").fadeToggle();
+	});
+
+	$(document).on("click", function (e) {
+		if ($("#lang").is(":visible") && $(e.target).closest("#lang").length == 0 && $(e.target).closest("#lang-switch").length == 0) {
+			$("#lang").fadeOut();
+		}
+	});
+
+	$(window).on("keydown", function (e) {
+		if ($("#lang").is(":visible") && e.keyCode == 27) {
+			$("#lang").fadeOut();
+		}
 	});
 });
 
