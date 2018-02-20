ad35fe256f1c200eb07b9d4eece80bc3cd88001f
John <daras.giannhs@gmail.com>
2018-02-20 23:20:17 +0200

    Css new theme first draft

--- resources/assets/js/home.js
+++ resources/assets/js/home.js
@@ -12,6 +12,17 @@ $(document).ready(function(){
 
 	if($("article.home").length > 0) {
 
+		$(window).on("scroll", _.debounce(function() {
+
+		var sh = $('.typewriter')[0].getBoundingClientRect().top;
+		if (sh<=0.56*$(window).height() && !($('.typewriter').hasClass('animated'))) {
+			$('.typewriter').addClass('animated');
+			typeWriter(); 
+		}
+
+
+		}))
+
 		$("body > header").addClass("home")
 
 		var l = Lorenz({
@@ -38,3 +49,16 @@ $(document).ready(function(){
 	}
 
 })
+
+var i = 0;
+var txt = '_What is '; /* The text */
+var speed = 250; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typer").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
