831a7f2a653f7fc45ff54ec3dcb5beffc5f546e1
Theo Diamantidis <diamaltho@gmail.com>
2018-02-22 14:47:28 +0200

    Fix tooltips & restore css

--- resources/assets/js/tooltip.js
+++ resources/assets/js/tooltip.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-	$(document).on("mouseover", "[data-tooltip]", function() {
+	$(document).on("mouseenter", "[data-tooltip]", function() {
 
 		var y = $(this)[0].getBoundingClientRect().top,
 			wh = $(window).height(),
+			ww = $(window).width(),
 			elh = $(this).outerHeight()
-		if(y + elh / 2 < wh / 2) {
+		/* enable only on < md screens */
+		if(ww < 768 && y + elh / 2 < wh / 2) {
 			$(this).removeClass("tooltip-top").addClass("tooltip-bottom")
 		} else {
 			$(this).removeClass("tooltip-bottom").addClass("tooltip-top")
