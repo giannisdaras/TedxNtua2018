32aa10c87b6cea184246466c8772f02c35196324
Theo Diamantidis <diamaltho@gmail.com>
2018-03-21 11:06:55 +0200

    Show speakers in order of appearance

--- app/Http/Controllers/SpeakersController.php
+++ app/Http/Controllers/SpeakersController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use LaravelLocalization;
 use App\Speaker;
 
 class SpeakersController extends Controller {
 
 	public function index(Request $request) {
+		Collection::macro('sortByAppearance', function() {
+			return $this->sortBy(function($item) {
+				/* $item->talk->hour will automatically return the hour attribute of the talk linked to the speaker */
+				return $item->talk->hour;
+			});
+		});
 
 		if(env('SHOW_HIDDEN_ENTRIES', false) == true) {
 			$query = new Speaker;
@@ -15,7 +22,7 @@ class SpeakersController extends Controller {
 			$query = Speaker::where('visible', true);
 		}
 
-		$speakers = $query->orderBy('name')->get();
+		$speakers = $query->get()->sortByAppearance();
 
 		$isPjax = $request->header('X-PJAX');
 		if ($isPjax) {
